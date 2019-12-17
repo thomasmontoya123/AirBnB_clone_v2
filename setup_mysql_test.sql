2d000dab88e747e4788551cd1f02c992ddf0b997
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-17 14:08:45 -0500

    typo on perfomance schema

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- prepares a MySQL server for the project test
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES on hbnb_dev_db. * to 'hbnb_test_db'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test_db'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
