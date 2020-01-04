10a20f28be489e11800aadf190409a91583d1ac1
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-03 20:38:14 -0500

    task0 deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+
+touch /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Hello Holberton School!
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -snf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+service nginx restart
