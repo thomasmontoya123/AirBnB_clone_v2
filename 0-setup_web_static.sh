32bb62b7970cb735ab5b7461f7c4ca2f543ec02b
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-03 20:40:46 -0500

    alias flag removed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,7 +13,7 @@ echo "<html>
     Hello Holberton School!
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -snf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
 service nginx restart
