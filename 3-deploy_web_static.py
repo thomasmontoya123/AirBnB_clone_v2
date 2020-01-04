f4a283b180d971b0e58274a3f1f36ca8c7c4114e
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-03 21:49:02 -0500

    task3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+creates and distributes an archive to your web servers,
+using the function deploy
+"""
+
+from datetime import datetime
+from fabric.operations import local, put, run
+from fabric.api import env
+from os import path
+
+env.hosts = ['35.190.151.31', '35.185.1.2']
+
+
+def do_pack():
+    '''Compressor function '''
+    local("mkdir -p versions")
+    archive = local("tar -zcvf versions/web_static_{}.tgz web_static".format(
+        datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+
+    archive_name = "versions/web_static_{}.tgz".format(
+        datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
+
+    if archive.failed:
+        return None
+    return archive_name
+
+
+def do_deploy(archive_path):
+    '''creates and distributes an archive to your web servers '''
+    if not path.exists(archive_path):
+        return False
+
+    try:
+        my_name = archive_path[9:]
+        my_name_short = archive_path[9:-4]
+        put(archive_path, "/tmp/{}".format(my_name))
+        run("mkdir -p /data/web_static/releases/{}/".format(my_name_short))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(my_name, my_name_short))
+        run("mv /data/web_static/releases/{}/web_static/*\
+                            /data/web_static/releases/{}/"
+            .format(my_name_short, my_name_short))
+        run("rm /tmp/{}".format(my_name))
+        run("rm -fr /data/web_static/current")
+        run("rm -fr /data/web_static/releases/{}/web_static"
+            .format(my_name_short))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(my_name_short))
+        print("New version deployed!")
+    except Exception:
+        return False
+
+
+def deploy():
+    '''full deployment'''
+    new = do_pack()
+    if not new:
+        return False
+    return do_deploy(new)
