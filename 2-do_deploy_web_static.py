b55b8ffd9518232f94aa484a490f944f942e63c1
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-03 21:25:03 -0500

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""
-Distributes an archive to the web servers,
-using the function do_deploy
-"""
-
+'''Distributes an archive to the web servers,
+using the function do_deploy'''
 from datetime import datetime
 from fabric.operations import local, put, run
 from fabric.api import env
@@ -24,29 +21,24 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    '''distributes an archive to your web servers'''
+    """ DEploys """
     if not path.exists(archive_path):
         return False
 
-    try:
-        full_name = archive_path[9:]
-        short_name = archive_path[9:-4]
+    full_name = archive_path[9:]
+    short_name = archive_path[9:-4]
 
+    try:
         put(archive_path, "/tmp/{}".format(full_name))
-        run("mkdir -p /data/web_static/releases/{}/".format(short_name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(full_name, short_name))
-        run("mv /data/web_static/releases/{}/web_static/*\
-                            /data/web_static/releases/{}/"
-            .format(short_name, short_name))
+        run("mkdir -p /data/web_static/releases/{}".format(short_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(full_name, short_name))
         run("rm /tmp/{}".format(full_name))
-        run("rm -fr /data/web_static/current")
-        run("rm -fr /data/web_static/releases/{}/web_static"
-            .format(short_name))
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(short_name))
+        run("mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/".format(short_name, short_name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(short_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(short_name))
         print("New version deployed!")
-        return True
 
     except Exception:
         return False
