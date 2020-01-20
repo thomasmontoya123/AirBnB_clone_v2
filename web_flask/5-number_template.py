821367220d8aa2aec91bca59c10c92d1fb3bd7bb
Thomas Montoya alvarez <thomas@Thomass-MacBook-Pro.local>
2020-01-20 18:42:12 -0500

    example decorator removed

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,10 +34,6 @@ def route_number(n):
 def route_number_template(n):
     return render_template('5-number.html', number=n)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
